a096923886eec03bea5ab373bd354bd3bc4e4593
Andrey Mudri <andreybeckert@gmail.com>
2023-05-14 23:13:06 -0300

    chore: adjustments for deploy.2

--- src/database/database.js
+++ src/database/database.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 const { Pool } = pkg;
 dotenv.config();
 
-if (process.env.MODE === "prod") configDatabase.ssl = true;
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+if (process.env.MODE === "prod") db.ssl = true;
 export default db;
